c2b14f9c2879742caa411d27d05fa682ea987bbb
Vlad Romascanu <ntvdm@hotmail.com>
2001-02-23 07:33:29 +0000

    Modified to reflect new SB modules

--- VDMSModules/Deployment/install.bat
+++ VDMSModules/Deployment/install.bat
@@ -1,5 +1,8 @@
 @echo Installing...
 @regsvr32 /c EmuMPU401.dll
+@regsvr32 /c EmuSBCompat.dll
+@regsvr32 /c DMAController.dll
 @regsvr32 /c MIDIDevice.dll
 @regsvr32 /c MIDIIndicator.dll
 @regsvr32 /c MIDIToolkit.dll
+@regsvr32 /c WaveDevice.dll
--- VDMSModules/Deployment/uninstall.bat
+++ VDMSModules/Deployment/uninstall.bat
@@ -1,5 +1,8 @@
 @echo Uninstalling...
 @regsvr32 /c /u EmuMPU401.dll
+@regsvr32 /c /u EmuSBCompat.dll
+@regsvr32 /c /u DMAController.dll
 @regsvr32 /c /u MIDIDevice.dll
 @regsvr32 /c /u MIDIIndicator.dll
 @regsvr32 /c /u MIDIToolkit.dll
+@regsvr32 /c /u WaveDevice.dll
