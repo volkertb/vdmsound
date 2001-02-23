b0d52ef5847a924d5b2d4342c815d92d1a0bd060
Vlad Romascanu <ntvdm@hotmail.com>
2001-02-23 07:30:34 +0000

    Added log filtering

--- VDMSCore/Sources/VDMConfig/CfgDispatch.h
+++ VDMSCore/Sources/VDMConfig/CfgDispatch.h
@@ -11,18 +11,20 @@ typedef std::map<std::string,IUnknownPtr,striLess> modulemap_t;
 struct CfgEnvironment {
   CfgEnvironment(
       const CfgEnvironment& src)
-    : name(src.name), config(src.config), modules(src.modules)
-    { };
+    : name(src.name), config(src.config), modules(src.modules), loggingLevel(src.loggingLevel)
+    { }
   CfgEnvironment(
       const std::string& _name,
       const CVDMConfig& _config,
-      const modulemap_t& _modules)
-    : name(_name), config(_config), modules(_modules)
-    { };
+      const modulemap_t& _modules,
+      int _loggingLevel )
+    : name(_name), config(_config), modules(_modules), loggingLevel(_loggingLevel)
+    { }
 
   const std::string& name;
   const CVDMConfig& config;
   const modulemap_t& modules;
+  const int loggingLevel;
 };
 
 #endif //__CFGDISPATCH_H_
