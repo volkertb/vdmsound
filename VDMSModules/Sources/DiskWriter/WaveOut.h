a0a11d112b605a09651846e3b7f2a67e296624c8
Vlad Romascanu <ntvdm@hotmail.com>
2001-02-27 21:45:51 +0000

    no message

---
+++ VDMSModules/Sources/DiskWriter/WaveOut.h
@@ -0,0 +1,62 @@
+// WaveOut.h : Declaration of the CWaveOut
+
+#ifndef __WAVEOUT_H_
+#define __WAVEOUT_H_
+
+#include "resource.h"       // main symbols
+
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma warning ( disable : 4192 )
+#import <IVDMModule.tlb> raw_interfaces_only, raw_native_types, no_namespace, named_guids 
+#import <IWave.tlb> raw_interfaces_only, raw_native_types, no_namespace, named_guids 
+
+#import <IVDMQuery.tlb>
+
+/////////////////////////////////////////////////////////////////////////////
+// CWaveOut
+class ATL_NO_VTABLE CWaveOut : 
+	public CComObjectRootEx<CComMultiThreadModel>,
+	public CComCoClass<CWaveOut, &CLSID_WaveOut>,
+	public ISupportErrorInfo,
+  public IVDMBasicModule,
+  public IWaveDataConsumer
+{
+public:
+	CWaveOut()
+	{	}
+
+DECLARE_REGISTRY_RESOURCEID(IDR_WAVEOUT)
+DECLARE_NOT_AGGREGATABLE(CWaveOut)
+
+DECLARE_PROTECT_FINAL_CONSTRUCT()
+
+BEGIN_COM_MAP(CWaveOut)
+  COM_INTERFACE_ENTRY(ISupportErrorInfo)
+  COM_INTERFACE_ENTRY(IVDMBasicModule)
+  COM_INTERFACE_ENTRY(IWaveDataConsumer)
+END_COM_MAP()
+
+// ISupportsErrorInfo
+public:
+  STDMETHOD(InterfaceSupportsErrorInfo)(REFIID riid);
+
+// IVDMBasicModule
+public:
+  STDMETHOD(Init)(IUnknown * configuration);
+  STDMETHOD(Destroy)();
+
+// IWaveDataConsumer
+public:
+  STDMETHOD(SetFormat)(WORD channels, DWORD samplesPerSec, WORD bitsPerSample);
+  STDMETHOD(PlayData)(BYTE * data, LONG length);
+
+protected:
+  FILE* m_fOut;
+
+protected:
+  IVDMQUERYLib::IVDMRTEnvironmentPtr m_env;
+  IWaveDataConsumerPtr m_waveOut;
+};
+
+#endif //__WAVEOUT_H_
