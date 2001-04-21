85ab774808f0cd1c9c1c9cc88c9a657a379aae95
vladr <vladr@e27a9115-32c9-4d73-9f06-1881319d875a>
2001-04-21 22:07:07 +0000

    Updated with new modules

--- trunk/VDMSModules/Deployment/install.bat
+++ trunk/VDMSModules/Deployment/install.bat
@@ -1,8 +1,10 @@
 @echo Installing...
 @regsvr32 /c EmuMPU401.dll
 @regsvr32 /c EmuSBCompat.dll
+@regsvr32 /c EmuJoystick.dll
 @regsvr32 /c DMAController.dll
 @regsvr32 /c MIDIDevice.dll
 @regsvr32 /c MIDIIndicator.dll
 @regsvr32 /c MIDIToolkit.dll
 @regsvr32 /c WaveDevice.dll
+@regsvr32 /c DiskWriter.dll
