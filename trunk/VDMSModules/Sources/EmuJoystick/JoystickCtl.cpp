4283662d517628afe7afc45f905a539d9550ec43
vladr <vladr@e27a9115-32c9-4d73-9f06-1881319d875a>
2001-04-22 15:24:06 +0000

    Fixed button negative logic bug Fixed coordinate scaling 0..65535 assumption Added asynchronous button state updating (new polling thread) Added VDMS.INI configurable parameters

--- trunk/VDMSModules/Sources/EmuJoystick/JoystickCtl.cpp
+++ trunk/VDMSModules/Sources/EmuJoystick/JoystickCtl.cpp
@@ -3,12 +3,25 @@
 #include "EmuJoystick.h"
 #include "JoystickCtl.h"
 
+/////////////////////////////////////////////////////////////////////////////
+
 /* TODO: put these in a .mc file or something */
 #define MSG_ERR_INTERFACE _T("The dependency module '%1' does not support the '%2' interface.%0")
 
 /////////////////////////////////////////////////////////////////////////////
 
 #define INI_STR_VDMSERVICES   L"VDMSrv"
+
+#define INI_STR_BASEPORT      L"port"
+#define INI_STR_SCALEMIN      L"minCoord"
+#define INI_STR_SCALEMAX      L"maxCoord"
+#define INI_STR_POLLINTERVAL  L"pollPeriod"
+
+/////////////////////////////////////////////////////////////////////////////
+
+#define CHANGE_BIT(var, bit, set) ((set) ? ((var) | (1 << (bit))) : ((var) & (~(1 << (bit)))))
+
+#define LIN_REMAP(oldVal, oldMin, oldMax, newMin, newMax) (((((oldVal) - (oldMin)) * ((newMax) - (newMin))) / ((oldMax) - (oldMin))) + (newMin))
 
 /////////////////////////////////////////////////////////////////////////////
 
@@ -17,9 +30,6 @@
 
 #include <VDMUtil.h>
 #pragma comment ( lib , "VDMUtil.lib" )
-
-#pragma comment ( lib , "winmm.lib" )
-
 
 /////////////////////////////////////////////////////////////////////////////
 // CJoystickCtl
@@ -30,17 +40,17 @@
 
 STDMETHODIMP CJoystickCtl::InterfaceSupportsErrorInfo(REFIID riid)
 {
-	static const IID* arr[] = 
-	{
-		&IID_IVDMBasicModule,
-		&IID_IIOHandler,
-	};
-	for (int i=0; i < sizeof(arr) / sizeof(arr[0]); i++)
-	{
-		if (InlineIsEqualGUID(*arr[i],riid))
-			return S_OK;
-	}
-	return S_FALSE;
+  static const IID* arr[] = 
+  {
+    &IID_IVDMBasicModule,
+    &IID_IIOHandler,
+  };
+  for (int i=0; i < sizeof(arr) / sizeof(arr[0]); i++)
+  {
+    if (InlineIsEqualGUID(*arr[i],riid))
+      return S_OK;
+  }
+  return S_FALSE;
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -48,83 +58,96 @@ STDMETHODIMP CJoystickCtl::InterfaceSupportsErrorInfo(REFIID riid)
 /////////////////////////////////////////////////////////////////////////////
 
 STDMETHODIMP CJoystickCtl::Init(IUnknown * configuration) {
-	if (configuration == NULL)
-		return E_POINTER;
+  if (configuration == NULL)
+    return E_POINTER;
 
-	HRESULT hr;
+  HRESULT hr;
 
-	IVDMQUERYLib::IVDMQueryDependenciesPtr Depends;   // Dependency query object
-	IVDMQUERYLib::IVDMQueryConfigurationPtr Config;   // Configuration query object
+  IVDMQUERYLib::IVDMQueryDependenciesPtr Depends;   // Dependency query object
+  IVDMQUERYLib::IVDMQueryConfigurationPtr Config;   // Configuration query object
 
-	// Grab a copy of the runtime environment (useful for logging, etc.)
-	RTE_Set(m_env, configuration);
+  // Grab a copy of the runtime environment (useful for logging, etc.)
+  RTE_Set(m_env, configuration);
 
-	// query how many joysticks are installed
-	m_NumJoysticks = joyGetNumDevs();
-	if (m_NumJoysticks == 0) { //no joystick -> do nothing
-		return S_OK;
-	}
+  // Initialize configuration and VDM services
+  try {
+    // Obtain the Query objects (for intialization purposes)
+    Depends    = configuration; // Dependency query object
+    Config     = configuration; // Configuration query object
 
-	// emulate 1 or 2 joysticks?
-	m_NumJoysticks = m_NumJoysticks >= 2 ? 2 : 1;
+    /** Get settings *******************************************************/
 
-	// Initialize configuration and VDM services
-	try {
-		// Obtain the Query objects (for intialization purposes)
-		Depends    = configuration; // Dependency query object
-		Config     = configuration; // Configuration query object
+    // Try to obtain the joystick settings, use defaults if none specified
+    m_basePort = CFG_Get(Config, INI_STR_BASEPORT, 0x201, 16, false);
+    m_scaleMin = CFG_Get(Config, INI_STR_SCALEMIN, 15, 10, false);
+    m_scaleMax = CFG_Get(Config, INI_STR_SCALEMAX, 575, 10, false);
+    m_pollInterval = CFG_Get(Config, INI_STR_POLLINTERVAL, 125, 10, false);
 
-		// Obtain VDM Services instance
-		IUnknownPtr VDMServices
-		           = Depends->Get(INI_STR_VDMSERVICES);
-		m_BaseSrv  = VDMServices;   // Base services (registers, interrupts, etc)
-		m_IOSrv    = VDMServices;   // I/O services (I/O port hooks)
+    /** Get VDM services ***************************************************/
 
-		if (m_BaseSrv == NULL)
-			return AtlReportError(GetObjectCLSID(), (LPCTSTR)::FormatMessage(MSG_ERR_INTERFACE, /*false, NULL, 0, */false, (LPCTSTR)CString(INI_STR_VDMSERVICES), _T("IVDMBaseServices")), __uuidof(IVDMBasicModule), E_NOINTERFACE);
-		if (m_IOSrv == NULL)
-			return AtlReportError(GetObjectCLSID(), (LPCTSTR)::FormatMessage(MSG_ERR_INTERFACE, /*false, NULL, 0, */false, (LPCTSTR)CString(INI_STR_VDMSERVICES), _T("IVDMIOServices")), __uuidof(IVDMBasicModule), E_NOINTERFACE);
+    // Obtain VDM Services instance
+    IUnknownPtr VDMServices
+               = Depends->Get(INI_STR_VDMSERVICES);
+    m_BaseSrv  = VDMServices;   // Base services (registers, interrupts, etc)
+    m_IOSrv    = VDMServices;   // I/O services (I/O port hooks)
 
-	} catch (_com_error& ce) {
-		SetErrorInfo(0, ce.ErrorInfo());
-		return ce.Error();          // Propagate the error
-	}
+    if (m_BaseSrv == NULL)
+      return AtlReportError(GetObjectCLSID(), (LPCTSTR)::FormatMessage(MSG_ERR_INTERFACE, /*false, NULL, 0, */false, (LPCTSTR)CString(INI_STR_VDMSERVICES), _T("IVDMBaseServices")), __uuidof(IVDMBasicModule), E_NOINTERFACE);
+    if (m_IOSrv == NULL)
+      return AtlReportError(GetObjectCLSID(), (LPCTSTR)::FormatMessage(MSG_ERR_INTERFACE, /*false, NULL, 0, */false, (LPCTSTR)CString(INI_STR_VDMSERVICES), _T("IVDMIOServices")), __uuidof(IVDMBasicModule), E_NOINTERFACE);
 
-	IVDMSERVICESLib::IIOHandler* pHandler = NULL; // IIOHandler interface to <this>
+  } catch (_com_error& ce) {
+    SetErrorInfo(0, ce.ErrorInfo());
+    return ce.Error();          // Propagate the error
+  }
 
-	// Register the I/O handlers
-	try {
-		// Obtain a COM IIOHandler interface on this C++ object
-		if (FAILED(hr = QueryInterface(__uuidof(IVDMSERVICESLib::IIOHandler), (void**)(&pHandler))))
-			throw _com_error(hr);     // Failure
+  IVDMSERVICESLib::IIOHandler* pHandler = NULL; // IIOHandler interface to <this>
 
-		// Add this object as an I/O handler on the specified port range
-		m_IOSrv->AddIOHook(0x201, 1, IVDMSERVICESLib::OP_SINGLE_BYTE, IVDMSERVICESLib::OP_SINGLE_BYTE, pHandler);
+  // Register the I/O handlers
+  try {
+    // Obtain a COM IIOHandler interface on this C++ object
+    if (FAILED(hr = QueryInterface(__uuidof(IVDMSERVICESLib::IIOHandler), (void**)(&pHandler))))
+      throw _com_error(hr);     // Failure
 
-		pHandler->Release();        // Take back the AddRef in QueryInterface above
-	} catch (_com_error& ce) {
-		if (pHandler != NULL)
-			pHandler->Release();      // Release the (unused) interface
+    // Add this object as an I/O handler on the specified port range
+    m_IOSrv->AddIOHook(m_basePort, 1, IVDMSERVICESLib::OP_SINGLE_BYTE, IVDMSERVICESLib::OP_SINGLE_BYTE, pHandler);
 
-		SetErrorInfo(0, ce.ErrorInfo());
-		return ce.Error();          // Propagate the error
-	}
+    pHandler->Release();        // Take back the AddRef in QueryInterface above
+  } catch (_com_error& ce) {
+    if (pHandler != NULL)
+      pHandler->Release();      // Release the (unused) interface
 
-	RTE_RecordLogEntry(m_env, IVDMQUERYLib::LOG_INFORMATION, Format(_T("JoystickCtl initialized")));
+    SetErrorInfo(0, ce.ErrorInfo());
+    return ce.Error();          // Propagate the error
+  }
 
-	return S_OK;
+  // Put the joystick(s) in a known state
+  resetJoystick();
+
+  // Create the joystick driver polling thread (regularly updates the locally-cached joystick state)
+  m_joyPollThread.Create(this, true);
+  m_joyPollThread.SetPriority(THREAD_PRIORITY_NORMAL);
+  m_joyPollThread.Resume();
+
+  RTE_RecordLogEntry(m_env, IVDMQUERYLib::LOG_INFORMATION, Format(_T("JoystickCtl initialized (joystick A %s, joystick B %s)"), m_joyInfo[0].isPresent ? (LPCTSTR)Format(_T("detected as '%s'"), m_joyInfo[0].caps.szPname) : _T("not detected"), m_joyInfo[1].isPresent ? (LPCTSTR)Format(_T("detected as '%s'"), m_joyInfo[1].caps.szPname) : _T("not detected")));
+
+  return S_OK;
 }
 
 STDMETHODIMP CJoystickCtl::Destroy() {
-	// Release the VDM Services module
-	m_IOSrv   = NULL;
-	m_BaseSrv = NULL;
+  // Signal the polling thread to quit
+  if (m_joyPollThread.GetThreadHandle() != NULL)
+    m_joyPollThread.Cancel();
 
-	// Release the runtime environment
-	RTE_RecordLogEntry(m_env, IVDMQUERYLib::LOG_INFORMATION, Format(_T("JoystickCtl  released")));
-	RTE_Set(m_env, NULL);
+  // Release the VDM Services module
+  m_IOSrv   = NULL;
+  m_BaseSrv = NULL;
 
-	return S_OK;
+  // Release the runtime environment
+  RTE_RecordLogEntry(m_env, IVDMQUERYLib::LOG_INFORMATION, Format(_T("JoystickCtl  released")));
+  RTE_Set(m_env, NULL);
+
+  return S_OK;
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -132,144 +155,205 @@ STDMETHODIMP CJoystickCtl::Destroy() {
 /////////////////////////////////////////////////////////////////////////////
 
 STDMETHODIMP CJoystickCtl::HandleINB(USHORT inPort, BYTE * data) {
-	if (data == NULL)
-		return E_POINTER;
+  if (data == NULL)
+    return E_POINTER;
 
-	if (inPort != 0x201) { //not the joystick port
-		*data = 0xFF;
-		return E_FAIL;
-	}
+  switch (inPort - m_basePort) {
+    case 0:
+  // return button state / axis bits
+  /* format of the byte to be returned:
+                        +-------------------------------+
+                        | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+                        +-------------------------------+
+                          |   |   |   |   |   |   |   |
+  Joystick B, Button 2 ---+   |   |   |   |   |   |   +--- Joystick A, X Axis
+  Joystick B, Button 1 -------+   |   |   |   |   +------- Joystick A, Y Axis
+  Joystick A, Button 2 -----------+   |   |   +----------- Joystick B, X Axis
+  Joystick A, Button 1 ---------------+   +--------------- Joystick B, Y Axis */
 
-	// return button state / axis bits
-	/* format of the byte to be returned:
-	                      +-------------------------------+
-	                      | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
-	                      +-------------------------------+
-	                        |   |   |   |   |   |   |   |
-	Joystick B, Button 2 ---+   |   |   |   |   |   |   +--- Joystick A, X Axis
-	Joystick B, Button 1 -------+   |   |   |   |   +------- Joystick A, Y Axis
-	Joystick A, Button 2 -----------+   |   |   +----------- Joystick B, X Axis
-	Joystick A, Button 1 ---------------+   +--------------- Joystick B, Y Axis
+      *data = 0xff;
 
-	for now, the joystick values are only read when the program
-	writes to the port, otherwise we would have to call joyGetPos
-	to often. maybe we should update the status of the buttons every
-	other call or in less frequent interval with the reads....
+      if (m_joyInfo[0].isPresent) {
+        *data = (BYTE)CHANGE_BIT(*data, 0, m_joyInfo[0].XCount > 0);
+        *data = (BYTE)CHANGE_BIT(*data, 1, m_joyInfo[0].YCount > 0);
+        *data = (BYTE)CHANGE_BIT(*data, 4, !m_joyInfo[0].but1);
+        *data = (BYTE)CHANGE_BIT(*data, 5, !m_joyInfo[0].but2);
 
-	*/
-//	*data = m_J2Button2 << 7 ||
-//			m_J2Button1 << 6 ||
-//			m_J1Button2 << 5 ||
-//			m_J1Button1 << 4;
+        if (m_joyInfo[0].XCount > 0) m_joyInfo[0].XCount--;
+        if (m_joyInfo[0].YCount > 0) m_joyInfo[0].YCount--;
+      }
 
-	*data = 0;
+      if (m_joyInfo[1].isPresent) {
+        *data = (BYTE)CHANGE_BIT(*data, 2, m_joyInfo[1].XCount > 0);
+        *data = (BYTE)CHANGE_BIT(*data, 3, m_joyInfo[1].YCount > 0);
+        *data = (BYTE)CHANGE_BIT(*data, 6, !m_joyInfo[1].but1);
+        *data = (BYTE)CHANGE_BIT(*data, 7, !m_joyInfo[1].but2);
 
-	if (m_J2Button2 != 0)
-		*data |= 0x80;
+        if (m_joyInfo[1].XCount > 0) m_joyInfo[1].XCount--;
+        if (m_joyInfo[1].YCount > 0) m_joyInfo[1].YCount--;
+      }
 
-	if (m_J2Button1 != 0)
-		*data |= 0x40;
+      return S_OK;
 
-	if (m_J1Button2 != 0)
-		*data |= 0x20;
-
-	if (m_J1Button1 != 0)
-		*data |= 0x10;
-
-	if (m_J2YCountdown != 0) {
-		m_J2YCountdown--;
-		*data |= 0x08;
-	}
-
-	if (m_J2XCountdown != 0) {
-		m_J2XCountdown--;
-		*data |= 0x04;
-	}
-
-	if (m_J1YCountdown != 0) {
-		m_J1YCountdown--;
-		*data |= 0x02;
-	}
-
-	if (m_J1XCountdown != 0) {
-		m_J1XCountdown--;
-		*data |= 0x01;
-	}
-//	RTE_RecordLogEntry(m_env, IVDMQUERYLib::LOG_ERROR, Format(_T("Byte sent: %x"), *data));
-	return S_OK;
+    default:
+      *data = 0xff;
+      return E_FAIL;
+  }
 }
 
 STDMETHODIMP CJoystickCtl::HandleOUTB(USHORT outPort, BYTE data) {
-	JOYINFO info;
-	if (outPort != 0x201)
-		return E_FAIL;
+  switch (outPort - m_basePort) {
+    case 0:
+      m_mutex.Lock();         // gain exclusive access to joystick state
+      updateJoyState(JOYSTICKID1, m_joyInfo[0], true, true);  // update information about
+      updateJoyState(JOYSTICKID2, m_joyInfo[1], true, true);  //  both buttons and coordinates.
+      m_mutex.Unlock();       // release joystick state after having updated it
 
-	// ! WARNING !
-	// i assume that joystick coordinates will be in the range of
-	// 0 to 65535 (the variables are named w*pos -> they should
-	// be words
+      return S_OK;
 
-	// the range ist mapped to 0 - 900 so old games don't get
-	// problems because of to high values
-	RTE_RecordLogEntry(m_env, IVDMQUERYLib::LOG_ERROR, Format(_T("written to joystick port")));
-
-	switch (m_NumJoysticks) {
-	case 2:
-		if (joyGetPos(JOYSTICKID2, &info) == JOYERR_NOERROR) {
-			m_J2Button1 = (info.wButtons & JOY_BUTTON1) != 0? 1 : 0;
-			m_J2Button2 = (info.wButtons & JOY_BUTTON2) != 0? 1 : 0;
-			m_J2XCountdown = (info.wXpos * 900) / 65535;
-			m_J2YCountdown = (info.wYpos * 900) / 65535;
-		} else {
-			m_J2Button1 = 0;
-			m_J2Button2 = 0;
-			m_J2XCountdown = 0;
-			m_J2YCountdown = 0;
-		}
-	case 1:
-		if (joyGetPos(JOYSTICKID1, &info) == JOYERR_NOERROR) {
-			m_J1Button1 = (info.wButtons & JOY_BUTTON1) != 0? 1 : 0;
-			m_J1Button2 = (info.wButtons & JOY_BUTTON2) != 0? 1 : 0;
-			m_J1XCountdown = (info.wXpos * 900) / 65535;
-			m_J1YCountdown = (info.wYpos * 900) / 65535;
-		} else {
-			m_J1Button1 = 0;
-			m_J1Button2 = 0;
-			m_J1XCountdown = 0;
-			m_J1YCountdown = 0;
-		}
-	}
-	return S_OK;		
+    default:
+      return E_FAIL;
+  }
 }
 
 
 // ** BEGIN not implemented ** //////////////////////////////////////////////
 
 STDMETHODIMP CJoystickCtl::HandleINW(USHORT inPort, USHORT * data) {
-	if (data == NULL) return E_POINTER;
-	*data = -1;
-	return E_NOTIMPL;
+  if (data == NULL) return E_POINTER;
+  *data = -1;
+  return E_NOTIMPL;
 }
 STDMETHODIMP CJoystickCtl::HandleINSB(USHORT inPort, BYTE * data, USHORT count, DIR_T direction) {
-	if (data == NULL) return E_POINTER;
-	memset(data, -1, count * sizeof(data[0]));
-	return E_NOTIMPL;
+  if (data == NULL) return E_POINTER;
+  memset(data, -1, count * sizeof(data[0]));
+  return E_NOTIMPL;
 }
 STDMETHODIMP CJoystickCtl::HandleINSW(USHORT inPort, USHORT * data, USHORT count, DIR_T direction) {
-	if (data == NULL) return E_POINTER;
-	memset(data, -1, count * sizeof(data[0]));
-	return E_NOTIMPL;
+  if (data == NULL) return E_POINTER;
+  memset(data, -1, count * sizeof(data[0]));
+  return E_NOTIMPL;
 }
 STDMETHODIMP CJoystickCtl::HandleOUTW(USHORT outPort, USHORT data) {
-	return E_NOTIMPL;
+  return E_NOTIMPL;
 }
 STDMETHODIMP CJoystickCtl::HandleOUTSB(USHORT outPort, BYTE * data, USHORT count, DIR_T direction) {
-	if (data == NULL) return E_POINTER;
-	return E_NOTIMPL;
+  if (data == NULL) return E_POINTER;
+  return E_NOTIMPL;
 }
 STDMETHODIMP CJoystickCtl::HandleOUTSW(USHORT outPort, USHORT * data, USHORT count, DIR_T direction) {
-	if (data == NULL) return E_POINTER;
-	return E_NOTIMPL;
+  if (data == NULL) return E_POINTER;
+  return E_NOTIMPL;
 }
 
 // ** END not implemented ** ////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+// IRunnable
+/////////////////////////////////////////////////////////////////////////////
+
+unsigned int CJoystickCtl::Run(CThread& thread) {
+  MSG message;
+
+  _ASSERTE(thread.GetThreadID() == m_joyPollThread.GetThreadID());
+
+  RTE_RecordLogEntry(m_env, IVDMQUERYLib::LOG_INFORMATION, Format(_T("Polling thread created (handle = 0x%08x, ID = %d)"), (int)thread.GetThreadHandle(), (int)thread.GetThreadID()));
+
+  do {
+    if (thread.GetMessage(&message, false)) {       // non-blocking (message 'peek')
+      switch (message.message) {
+        case WM_QUIT:
+          RTE_RecordLogEntry(m_env, IVDMQUERYLib::LOG_INFORMATION, Format(_T("Polling thread cancelled")));
+          return 0;
+
+        default:
+          break;
+      }
+    } else {
+      m_mutex.Lock();         // gain exclusive access to joystick state
+      updateJoyState(JOYSTICKID1, m_joyInfo[0], false, true); // update the button information,
+      updateJoyState(JOYSTICKID2, m_joyInfo[1], false, true); //  but not the coordinate data.
+      m_mutex.Unlock();       // release joystick state after having updated it
+
+      Sleep((DWORD)m_pollInterval);
+    }
+  } while (true);
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Utility functions
+/////////////////////////////////////////////////////////////////////////////
+
+//
+// Reset the joystick state (all capacitors dicharged, all buttons released),
+//   and reload calibration data (min/max X-Y information, etc.)
+//
+void CJoystickCtl::resetJoystick(void) {
+  // Refresh joystick calibration data
+  updateJoyCaps(JOYSTICKID1, m_joyInfo[0]);
+  updateJoyCaps(JOYSTICKID2, m_joyInfo[1]);
+
+  // Pretend the buttons are not pressed
+  m_joyInfo[0].but1 = false;
+  m_joyInfo[0].but2 = false;
+  m_joyInfo[1].but1 = false;
+  m_joyInfo[1].but2 = false;
+
+  // Pretend the capacitors have discharged by now, i.e. the counters expired
+  m_joyInfo[0].XCount = 0;
+  m_joyInfo[0].YCount = 0;
+  m_joyInfo[1].XCount = 0;
+  m_joyInfo[1].YCount = 0;
+}
+
+//
+// Retrieves calibration data from the Windows driver
+//
+MMRESULT CJoystickCtl::updateJoyCaps(
+  UINT joyID,           // Identifier of the joystick (JOYSTICKID1 or JOYSTICKID2) to be queried
+  JoyInfo& joyInfo )    // Structure to contain the capabilities of the physical joystick
+{
+  MMRESULT errCode;
+
+  if ((errCode = joyGetDevCaps(joyID, &(joyInfo.caps), sizeof(joyInfo.caps))) != JOYERR_NOERROR) {
+    joyInfo.isPresent = false;
+  } else {
+    joyInfo.isPresent = true;
+  }
+
+  return errCode;
+}
+
+//
+//
+//
+MMRESULT CJoystickCtl::updateJoyState(
+  UINT joyID,           // Identifier of the joystick (JOYSTICKID1 or JOYSTICKID2) to be queried
+  JoyInfo& joyInfo,     // Structure to contain the *physical* position and button status of the joystick
+  bool doCoords,        // Indicates whether to update *emulated* coordinate information
+  bool doButtons )      // Indicates whether to update *emulated* button information
+{
+  MMRESULT errCode;
+
+  if ((errCode = joyGetPos(joyID, &(joyInfo.state))) != JOYERR_NOERROR) {
+    joyInfo.isPresent = false;
+  } else {
+    joyInfo.isPresent = true;
+
+    if (doCoords) {
+      joyInfo.XCount = LIN_REMAP((int)joyInfo.state.wXpos, joyInfo.caps.wXmin, joyInfo.caps.wXmax, m_scaleMin, m_scaleMax);
+      joyInfo.YCount = LIN_REMAP((int)joyInfo.state.wYpos, joyInfo.caps.wYmin, joyInfo.caps.wYmax, m_scaleMin, m_scaleMax);
+    }
+
+    if (doButtons) {
+      joyInfo.but1 = (joyInfo.state.wButtons & JOY_BUTTON1) != 0;
+      joyInfo.but2 = (joyInfo.state.wButtons & JOY_BUTTON2) != 0;
+    }
+  }
+
+  return errCode;
+}
--- trunk/VDMSModules/Sources/EmuJoystick/JoystickCtl.h
+++ trunk/VDMSModules/Sources/EmuJoystick/JoystickCtl.h
@@ -5,7 +5,7 @@
 
 #include "resource.h"       // Hauptsymbole
 
-
+/////////////////////////////////////////////////////////////////////////////
 
 #pragma warning ( disable : 4192 )
 #import <IVDMModule.tlb> raw_interfaces_only, raw_native_types, no_namespace, named_guids 
@@ -13,27 +13,33 @@
 
 #import <IVDMServices.tlb>
 #import <IVDMQuery.tlb>
+
+/////////////////////////////////////////////////////////////////////////////
+
+#include <Thread.h>
+
 /////////////////////////////////////////////////////////////////////////////
 // CJoystickCtl
 class ATL_NO_VTABLE CJoystickCtl : 
-	public CComObjectRootEx<CComMultiThreadModel>,
-	public CComCoClass<CJoystickCtl, &CLSID_JoystickCtl>,
-	public ISupportErrorInfo,
-	public IVDMBasicModule,
-	public IIOHandler
+  public CComObjectRootEx<CComMultiThreadModel>,
+  public CComCoClass<CJoystickCtl, &CLSID_JoystickCtl>,
+  public IRunnable,
+  public ISupportErrorInfo,
+  public IVDMBasicModule,
+  public IIOHandler
 {
+protected:
+  struct JoyInfo {
+    bool isPresent;     // is the joystick available ?
+    int XCount, YCount; // (X,Y) position counters
+    bool but1, but2;    // joystick buttons state
+    JOYCAPS caps;       // Windows device capabilities
+    JOYINFO state;      // latest device data from Windows
+  };
+
 public:
-	CJoystickCtl()
-	{
-		m_J1XCountdown = 0;
-		m_J1YCountdown = 0;
-		m_J2XCountdown = 0;
-		m_J2YCountdown = 0;
-		m_J1Button1 = 0;
-		m_J1Button2 = 0;
-		m_J2Button1 = 0;
-		m_J2Button2 = 0;
-	}
+  CJoystickCtl()
+    { }
 
 DECLARE_REGISTRY_RESOURCEID(IDR_JOYSTICKCTL)
 DECLARE_NOT_AGGREGATABLE(CJoystickCtl)
@@ -43,37 +49,52 @@ DECLARE_PROTECT_FINAL_CONSTRUCT()
 BEGIN_COM_MAP(CJoystickCtl)
   COM_INTERFACE_ENTRY(IVDMBasicModule)
   COM_INTERFACE_ENTRY(IIOHandler)
-	COM_INTERFACE_ENTRY(ISupportErrorInfo)
+  COM_INTERFACE_ENTRY(ISupportErrorInfo)
 END_COM_MAP()
+
+// IRunnable
+public:
+  unsigned int Run(CThread& thread);
 
 // ISupportsErrorInfo
 public:
-	STDMETHOD(InterfaceSupportsErrorInfo)(REFIID riid);
+  STDMETHOD(InterfaceSupportsErrorInfo)(REFIID riid);
 
 // IVDMBasicModule
 public:
-	STDMETHOD(Init)(IUnknown * configuration);
-	STDMETHOD(Destroy)();
+  STDMETHOD(Init)(IUnknown * configuration);
+  STDMETHOD(Destroy)();
 
 // IIOHandler
 public:
-	STDMETHOD(HandleINB)(USHORT inPort, BYTE * data);
-	STDMETHOD(HandleINW)(USHORT inPort, USHORT * data);
-	STDMETHOD(HandleINSB)(USHORT inPort, BYTE * data, USHORT count, DIR_T direction);
-	STDMETHOD(HandleINSW)(USHORT inPort, USHORT * data, USHORT count, DIR_T direction);
-	STDMETHOD(HandleOUTB)(USHORT outPort, BYTE data);
-	STDMETHOD(HandleOUTW)(USHORT outPort, USHORT data);
-	STDMETHOD(HandleOUTSB)(USHORT outPort, BYTE * data, USHORT count, DIR_T direction);
-	STDMETHOD(HandleOUTSW)(USHORT outPort, USHORT * data, USHORT count, DIR_T direction);
+  STDMETHOD(HandleINB)(USHORT inPort, BYTE * data);
+  STDMETHOD(HandleINW)(USHORT inPort, USHORT * data);
+  STDMETHOD(HandleINSB)(USHORT inPort, BYTE * data, USHORT count, DIR_T direction);
+  STDMETHOD(HandleINSW)(USHORT inPort, USHORT * data, USHORT count, DIR_T direction);
+  STDMETHOD(HandleOUTB)(USHORT outPort, BYTE data);
+  STDMETHOD(HandleOUTW)(USHORT outPort, USHORT data);
+  STDMETHOD(HandleOUTSB)(USHORT outPort, BYTE * data, USHORT count, DIR_T direction);
+  STDMETHOD(HandleOUTSW)(USHORT outPort, USHORT * data, USHORT count, DIR_T direction);
 
 protected:
-	IVDMQUERYLib::IVDMRTEnvironmentPtr m_env;
-	IVDMSERVICESLib::IVDMBaseServicesPtr m_BaseSrv;
-	IVDMSERVICESLib::IVDMIOServicesPtr m_IOSrv;
+  void resetJoystick(void);
+  MMRESULT updateJoyCaps(UINT joyID, JoyInfo& joyInfo);
+  MMRESULT updateJoyState(UINT joyID, JoyInfo& joyInfo, bool doCoords = true, bool doButtons = true);
 
-	int m_NumJoysticks;
-	int m_J1XCountdown, m_J1YCountdown, m_J2XCountdown, m_J2YCountdown;
-	int m_J1Button1, m_J1Button2, m_J2Button1, m_J2Button2;
+protected:
+  int m_basePort;             // base port where the joystick can be accessed by DOS programs
+  int m_scaleMin, m_scaleMax; // range of values for the X and Y intervals reported to DOS programs
+  int m_pollInterval;         // how many milliseconds to sleep between joystick polls
+
+protected:
+  CThread m_joyPollThread;    // thread that regularly polls the Windows driver
+  CCriticalSection m_mutex;   // regulates concurrent access to the device data in m_joyInfo (regulate access by the poll thread + forced access by some I/O traps)
+  JoyInfo m_joyInfo[2];       // joystick information
+
+protected:
+  IVDMQUERYLib::IVDMRTEnvironmentPtr m_env;
+  IVDMSERVICESLib::IVDMBaseServicesPtr m_BaseSrv;
+  IVDMSERVICESLib::IVDMIOServicesPtr m_IOSrv;
 };
 
 #endif //__JOYSTICKCTL_H_
