0c6868bce118e31b98c9dfbe553cb2581b2e6069
Vlad Romascanu <ntvdm@hotmail.com>
2001-05-06 04:43:42 +0000

    Added AdLib emulation module

---
+++ VDMSModules/Sources/EmuAdLib/EmuAdLib.cpp
@@ -0,0 +1,95 @@
+// EmuAdLib.cpp : Implementation of DLL Exports.
+
+
+// Note: Proxy/Stub Information
+//      To build a separate proxy/stub DLL, 
+//      run nmake -f EmuAdLibps.mk in the project directory.
+
+#include "stdafx.h"
+#include "resource.h"
+#include <initguid.h>
+#include "EmuAdLib.h"
+
+#include "EmuAdLib_i.c"
+
+
+CComModule _Module;
+
+BEGIN_OBJECT_MAP(ObjectMap)
+END_OBJECT_MAP()
+
+class CEmuAdLibApp : public CWinApp
+{
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CEmuAdLibApp)
+	public:
+    virtual BOOL InitInstance();
+    virtual int ExitInstance();
+	//}}AFX_VIRTUAL
+
+	//{{AFX_MSG(CEmuAdLibApp)
+		// NOTE - the ClassWizard will add and remove member functions here.
+		//    DO NOT EDIT what you see in these blocks of generated code !
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+BEGIN_MESSAGE_MAP(CEmuAdLibApp, CWinApp)
+	//{{AFX_MSG_MAP(CEmuAdLibApp)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+		//    DO NOT EDIT what you see in these blocks of generated code!
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+CEmuAdLibApp theApp;
+
+BOOL CEmuAdLibApp::InitInstance()
+{
+    _Module.Init(ObjectMap, m_hInstance, &LIBID_EMUADLIBLib);
+    return CWinApp::InitInstance();
+}
+
+int CEmuAdLibApp::ExitInstance()
+{
+    _Module.Term();
+    return CWinApp::ExitInstance();
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Used to determine whether the DLL can be unloaded by OLE
+
+STDAPI DllCanUnloadNow(void)
+{
+    AFX_MANAGE_STATE(AfxGetStaticModuleState());
+    return (AfxDllCanUnloadNow()==S_OK && _Module.GetLockCount()==0) ? S_OK : S_FALSE;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Returns a class factory to create an object of the requested type
+
+STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
+{
+    return _Module.GetClassObject(rclsid, riid, ppv);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// DllRegisterServer - Adds entries to the system registry
+
+STDAPI DllRegisterServer(void)
+{
+    // registers object, typelib and all interfaces in typelib
+    return _Module.RegisterServer(TRUE);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// DllUnregisterServer - Removes entries from the system registry
+
+STDAPI DllUnregisterServer(void)
+{
+    return _Module.UnregisterServer(TRUE);
+}
+
+
