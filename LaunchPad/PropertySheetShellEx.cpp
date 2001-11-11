451244dd826580d9f7c347aee1ece39783363fea
Vlad Romascanu <ntvdm@hotmail.com>
2001-11-11 18:16:04 +0000

    Initial checkin

---
+++ LaunchPad/PropertySheetShellEx.cpp
@@ -0,0 +1,13 @@
+// PropertySheetShellEx.cpp : Implementation of CPropertySheetShellEx
+#include "stdafx.h"
+#include "LaunchPad.h"
+#include "PropertySheetShellEx.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// CPropertySheetShellEx
+
+HRESULT CPropertySheetShellEx::Initialize(LPCITEMIDLIST pidlFolder, LPDATAOBJECT pDataObj, HKEY hProgID) {
+  AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+  return E_NOTIMPL;
+}
