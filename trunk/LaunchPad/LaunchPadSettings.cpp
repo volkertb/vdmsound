01704a0aefde4169e70fb52f833f92fde1346763
vladr <vladr@e27a9115-32c9-4d73-9f06-1881319d875a>
2002-01-28 04:13:18 +0000

    Added new functionality (string encoding, settings cloning)

--- trunk/LaunchPad/LaunchPadSettings.cpp
+++ trunk/LaunchPad/LaunchPadSettings.cpp
@@ -8,7 +8,11 @@ static char THIS_FILE[]=__FILE__;
 #define new DEBUG_NEW
 #endif
 
+//////////////////////////////////////////////////////////////////////
 
+#define TMP_BUF_SIZE  16384
+#define META_CHAR     '%'
+#define COMMENT_CHAR  ';'
 
 //////////////////////////////////////////////////////////////////////
 //
@@ -30,6 +34,11 @@ CLaunchPadSettings::CLaunchPadSettings(const CString& fileName) {
 CLaunchPadSettings::CLaunchPadSettings(const CStringArray& fileNames) {
   // Keep a copy of the list of file names the settings are stored in
   m_fileNames.Copy(fileNames);
+}
+
+CLaunchPadSettings::CLaunchPadSettings(const CLaunchPadSettings& settings) {
+  // Keep a copy of the list of file names the settings are stored in
+  *this = settings;
 }
 
 CLaunchPadSettings::~CLaunchPadSettings(void) {
@@ -71,15 +80,8 @@ HRESULT CLaunchPadSettings::GetValue(
 
     // Go through all configuration files
     for (int i = 0; i < m_fileNames.GetSize(); i++) {
-      LPTSTR szTmpBuf = tmpBuf.GetBuffer(1024);       // obtain direct access to tmpBuf's internal storage (reserve 1024 characters)
-
-      GetPrivateProfileString(section, key, defValue, szTmpBuf, 1024, m_fileNames.GetAt(i));
-
-      tmpBuf.ReleaseBuffer();                         // give tmpBuf back control of its internal storage
-
-      tmpBuf.Replace(_T(';'), _T('\0'));              // remove ';'-prefixed comments
-      tmpBuf.TrimLeft();                              // trim spaces
-      tmpBuf.TrimRight();                             // trim spaces
+      if (!GetPrivateProfileString(section, key, defValue, tmpBuf, m_fileNames.GetAt(i)))
+        return E_FAIL;
 
       if (isFirstValue) {
         isFirstValue = false;
@@ -213,4 +215,149 @@ HRESULT CLaunchPadSettings::CommitAll(void) {
   }
 
   return retVal;
+}
+
+
+//
+// Check whether a certain setting changed
+//
+BOOL CLaunchPadSettings::IsChanged(LPCTSTR section, LPCTSTR key) {
+  SettingKey cacheKey(section, key);                  // the key used to access this setting in the settings cache
+  SettingValue cacheValue(FALSE, FALSE);              // the cached setting value
+
+  if (!m_settingsCache.Lookup(cacheKey, cacheValue)) {
+    return FALSE;
+  } else {
+    return cacheValue.m_isChanged;
+  }
+}
+
+//
+// Check whether any setting changed
+//
+BOOL CLaunchPadSettings::IsChanged(void) {
+  SettingKey cacheKey;                                // temporary storage for the setting key
+  SettingValue cacheValue;                            // temporary storage for the setting value
+
+  POSITION pos = m_settingsCache.GetStartPosition();
+
+  while (pos != NULL) {
+    m_settingsCache.GetNextAssoc(pos, cacheKey, cacheValue);
+
+    if (cacheValue.m_isChanged) {
+      return TRUE;
+    }
+  }
+
+  return FALSE;
+}
+
+//
+//
+//
+CLaunchPadSettings& CLaunchPadSettings::operator =(const CLaunchPadSettings& src) {
+  // Keep a copy of the list of file names the settings are stored in
+  m_fileNames.Copy(src.m_fileNames);
+
+  // Keep a copy of the settings
+  SettingKey cacheKey;                                // temporary storage for the setting key
+  SettingValue cacheValue;                            // temporary storage for the setting value
+
+  POSITION pos = src.m_settingsCache.GetStartPosition();
+
+  while (pos != NULL) {
+    src.m_settingsCache.GetNextAssoc(pos, cacheKey, cacheValue);
+    m_settingsCache.SetAt(cacheKey, cacheValue);
+  }
+
+  return *this;
+}
+
+
+
+//////////////////////////////////////////////////////////////////////
+// Methods
+//////////////////////////////////////////////////////////////////////
+
+//
+//
+//
+BOOL CLaunchPadSettings::GetPrivateProfileString(LPCTSTR lpAppName, LPCTSTR lpKeyName, LPCTSTR lpDefault, CString& result, LPCTSTR lpFileName) {
+  int tmpStart, tmpStop;
+  CString tmpBuf;
+  WCHAR tmpWChr;
+# ifndef _UNICODE
+  CHAR tmpMBChr[8];
+# endif // _UNICODE
+
+  LPTSTR szResult = result.GetBuffer(TMP_BUF_SIZE);   // obtain direct access to result's internal storage (reserve TMP_BUF_SIZE characters)
+
+  ASSERT(szResult != NULL);
+
+  if (szResult == NULL)
+    return FALSE;
+
+  ::GetPrivateProfileString(lpAppName, lpKeyName, lpDefault, szResult, TMP_BUF_SIZE, lpFileName);
+
+  result.ReleaseBuffer();                             // give tmpBuf back control of its internal storage
+
+  for (int i = 0; i < result.GetLength(); i++) {
+    switch (result.GetAt(i)) {
+      case _T(META_CHAR):
+        tmpStart = min(i + 1, result.GetLength() - 1);
+        tmpStop  = min(i + 4, result.GetLength() - 1);
+        tmpBuf   = result.Mid(tmpStart, tmpStop - tmpStart + 1);
+        result.Delete(i, tmpStop - i + 1);
+        tmpWChr  = (WCHAR)_tcstoul(tmpBuf, NULL, 16);
+#       ifdef _UNICODE
+        result.Insert(i, tmpWChr);
+#       else  // _UNICODE
+        memset(tmpMBChr, 0, sizeof(tmpMBChr));
+        WideCharToMultiByte(CP_ACP, 0, &tmpWChr, 1, tmpMBChr, sizeof(tmpMBChr) - 1, NULL, NULL);
+        result.Insert(i, tmpMBChr);
+#       endif // _UNICODE
+        break;
+
+      case _T(COMMENT_CHAR):
+        result.Delete(i, result.GetLength() - i);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  result.TrimLeft();                                  // trim spaces
+  result.TrimRight();                                 // trim spaces
+
+  return TRUE;
+}
+
+//
+//
+//
+BOOL CLaunchPadSettings::WritePrivateProfileString(LPCTSTR lpAppName, LPCTSTR lpKeyName, LPCTSTR lpString, LPCTSTR lpFileName) {
+  CString tmpBuf1, tmpBuf2;
+
+  for (int i = 0; i < (int)_tcslen(lpString); i++) {
+    if (NeedsTranslation(lpString[i])) {
+      tmpBuf2.Format(_T("%c%04x"), _T(META_CHAR), lpString[i] & 0xffff);
+      tmpBuf1 += tmpBuf2;
+    } else {
+      tmpBuf1 += lpString[i];
+    }
+  }
+
+  return ::WritePrivateProfileString(lpAppName, lpKeyName, tmpBuf1, lpFileName);
+}
+
+//
+//
+//
+BOOL CLaunchPadSettings::NeedsTranslation(TCHAR tch) {
+  if ((tch < ' ') || (tch > '~') || (tch == COMMENT_CHAR) || (tch == META_CHAR)) {
+    return TRUE;
+  } else {
+    return FALSE;
+  }
 }
