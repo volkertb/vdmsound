f4ed5df9b3b3e19d5f1672d12f55a7e0b1560b8c
vladr <vladr@e27a9115-32c9-4d73-9f06-1881319d875a>
2002-02-04 03:25:30 +0000

    Initial checkin

---
+++ trunk/LaunchPad/AdvSettingsPage_Perf.cpp
@@ -0,0 +1,107 @@
+// AdvSettingsPage_Perf.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "resource.h"
+#include "AdvSettingsSheet.h"
+#include "AdvSettingsPage_Perf.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CAdvSettingsPage_Perf property page
+
+CAdvSettingsPage_Perf::CAdvSettingsPage_Perf(CLaunchPadSettings& settings)
+ : CPropertyPage(CAdvSettingsPage_Perf::IDD), m_settings(settings)
+{
+	//{{AFX_DATA_INIT(CAdvSettingsPage_Perf)
+		// NOTE: the ClassWizard will add member initialization here
+	//}}AFX_DATA_INIT
+}
+
+CAdvSettingsPage_Perf::~CAdvSettingsPage_Perf()
+{
+}
+
+void CAdvSettingsPage_Perf::DoDataExchange(CDataExchange* pDX)
+{
+	CPropertyPage::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CAdvSettingsPage_Perf)
+		// NOTE: the ClassWizard will add DDX and DDV calls here
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CAdvSettingsPage_Perf, CPropertyPage)
+	//{{AFX_MSG_MAP(CAdvSettingsPage_Perf)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+// CAdvSettingsPage helper functions
+
+VOID CAdvSettingsPage_Perf::SyncGUIData(BOOL bSave) {
+  //
+  // Synchronize the editable controls (checkboxes and radio buttons)
+  //  with the settings they represent
+  //
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// CAdvSettingsPage_Perf message handlers
+
+BOOL CAdvSettingsPage_Perf::OnInitDialog() 
+{
+	CPropertyPage::OnInitDialog();
+	
+  // Enable context help
+  m_help.Attach(this);
+
+  // Load the information from file into the GUI
+  SyncGUIData(FALSE);
+	
+	return TRUE;  // return TRUE unless you set the focus to a control
+	              // EXCEPTION: OCX Property Pages should return FALSE
+}
+
+BOOL CAdvSettingsPage_Perf::OnCommand(WPARAM wParam, LPARAM lParam) 
+{
+	UINT nID = LOWORD(wParam);
+	HWND hWndCtrl = (HWND)lParam;
+	int nCode = HIWORD(wParam);
+
+  ASSERT(::GetDlgItem(m_hWnd, nID) == hWndCtrl);
+/*
+  if ((GetDlgItem(nID) != NULL) &&
+      (nID != IDC_BUT_LOGBROWSE) && (nID != IDC_BUT_MPUOUTFILEBROWSE) &&
+      ((nCode == CBN_SELCHANGE) || (nCode == CBN_EDITCHANGE) || (nCode == BN_CLICKED)))
+  {
+    SetModified();              // enable the "Apply" button to reflect the fact that changes were made
+  }
+*/
+	return CPropertyPage::OnCommand(wParam, lParam);
+}
+
+BOOL CAdvSettingsPage_Perf::OnApply() 
+{
+  CWaitCursor wait;
+
+  SyncGUIData(TRUE);        // save all changes that occured in the GUI
+
+  CWnd* pWndParent = GetParent();
+
+  ASSERT_VALID(pWndParent);
+
+  if (pWndParent != NULL) {
+    pWndParent->SendMessage(WM_SAVESETTINGS);
+  }
+
+  return CPropertyPage::OnApply();
+}
---
+++ trunk/LaunchPad/AdvSettingsPage_Perf.h
@@ -0,0 +1,69 @@
+#if !defined(AFX_ADVSETTINGSPAGE_PERF_H__EEA4D190_5DB6_420E_BDB2_CE6A97C7E744__INCLUDED_)
+#define AFX_ADVSETTINGSPAGE_PERF_H__EEA4D190_5DB6_420E_BDB2_CE6A97C7E744__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// AdvSettingsPage_Perf.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+
+#include "LaunchPadSettings.h"
+#include "LaunchPadUtil.h"
+#include "ContextHelp.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// CAdvSettingsPage_Perf dialog
+
+class CAdvSettingsPage_Perf : public CPropertyPage
+{
+// Construction
+public:
+	CAdvSettingsPage_Perf(CLaunchPadSettings& settings);
+	~CAdvSettingsPage_Perf();
+
+// Helper functions
+protected:
+  VOID SyncGUIData(BOOL bSave = TRUE);
+
+// Member variables
+protected:
+  CLaunchPadSettings& m_settings;
+  CContextHelp m_help;
+
+// Public MFC stuff
+public:
+
+// Dialog Data
+	//{{AFX_DATA(CAdvSettingsPage_Perf)
+	enum { IDD = IDD_ADVPROPPAGE_PERF };
+		// NOTE - ClassWizard will add data members here.
+		//    DO NOT EDIT what you see in these blocks of generated code !
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generate virtual function overrides
+	//{{AFX_VIRTUAL(CAdvSettingsPage_Perf)
+	public:
+	virtual BOOL OnApply();
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	virtual BOOL OnCommand(WPARAM wParam, LPARAM lParam);
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	// Generated message map functions
+	//{{AFX_MSG(CAdvSettingsPage_Perf)
+	virtual BOOL OnInitDialog();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_ADVSETTINGSPAGE_PERF_H__EEA4D190_5DB6_420E_BDB2_CE6A97C7E744__INCLUDED_)
