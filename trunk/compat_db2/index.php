ebb63124dfc3dbdf952633c3dc4227c642af1663
lordwo <lordwo@e27a9115-32c9-4d73-9f06-1881319d875a>
2003-05-11 19:26:12 +0000

    Initial import. Some design basicly...

---
+++ trunk/compat_db2/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+
+<head>
+	<title>Welcome to the VOGONS DOS COMPATIBILITY DATABASE</title>
+    <link rel="stylesheet" href="inc/default.css" type="text/css">
+	
+</head>
+
+<body>
+
+<div class="table1"><div class="table2" style="padding-bottom: 24px; font-size: 8pt; font-family: arial black, arial;">VOGONS COMPATIBILITY DATABASE</div></div>
+
+<table width="100%" border="0" cellspacing="0" cellpadding="0" style="margin-top: 15px;">
+<tr>
+<td width="200">
+<!-- Menu -->
+<div class="table1"><div class="table2" style="font-family: courier, arial; padding-bottom: 24px;">
+<span style="padding-bottom: 24px; font-size: 8pt; font-family: arial black, arial;">C:\MENU.EXE</span>
+<hr>
+[ logged as ]
+<br>
+<br><b>o</b> My profile
+<br><b>o</b> My reports
+<br><b>o</b> Login/Logout
+<br><b>o</b> Register
+
+</div></div>
+
+<br>
+
+<!-- Database -->
+<div class="table1"><div class="table2" style="font-family: courier, arial; padding-bottom: 24px;">
+<span style="padding-bottom: 24px; font-size: 8pt; font-family: arial black, arial;">C:\REPORTS\DATABASE.EXE</span>
+<hr><center>
+# A B C D E F G H<br> 
+I J K L M N O P Q<br>
+R S T U V W X Y Z<br></center>
+<br>
+ 
+<u>Search:</u>&nbsp;&nbsp;<input style="border: 1px solid #000000; width: 120px; font-family: courier, arial;" value="needle">
+
+</div></div>
+
+<br>
+
+<!-- Small report history -->
+<div class="table1"><div class="table2" style="font-family: courier, arial; padding-bottom: 24px;">
+<span style="padding-bottom: 24px; font-size: 8pt; font-family: arial black, arial;">C:\REPORTS\LATEST.EXE</span><hr>
+<b>o</b> <i>Day of the tentacle</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><br>
+</div></div>
+
+<br>
+
+<!-- Weblinks -->
+<div class="table1"><div class="table2" style="font-family: courier, arial; padding-bottom: 24px;">
+<span style="padding-bottom: 24px; font-size: 8pt; font-family: arial black, arial;">C:\WEB\LINKS.EXE</span><hr>
+<b>o</b> <i>VOGONS</i>
+<br><b>o</b> <i>Webmaster 1</i>
+<br><b>o</b> <i>Webmaster 2</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><b>o</b> <i>Whatever...</i>
+<br><br>
+</div></div>
+</td>
+
+<!-- Content / Newsfeed -->
+<td valign="top">
+
+<div class="table1" style="margin-left: 10px;"><div class="table2" style="font-family: courier, arial; padding-bottom: 24px;">
+<span style="padding-bottom: 24px; font-size: 8pt; font-family: arial black, arial;">C:\REPORTS\NEWS.EXE --AUTHOR="VLADR" --TITLE="HELLO !"</span><hr>
+This is bullshit news..
+</div></div>
+</td>
+
+</tr></table>
+<br><br>
+<center>
+<a href="http://validator.w3.org/check/referer"><img border="0"
+          src="http://www.w3.org/Icons/valid-html40"
+          alt="Valid HTML 4.0!" height="31" width="88"></a>
+<a href="http://jigsaw.w3.org/css-validator/">
+  <img style="border:0;width:88px;height:31px"
+       src="http://jigsaw.w3.org/css-validator/images/vcss" 
+       alt="Valid CSS!">
+ </a><br>
+
+<span style="font-size: 10pt; font-family: arial;"><b>No bullshit here, the document is really valid.</b><br>Created under 1st page 2000 (http://www.evrsoft.com), a very good freeware editor !</span>
+</center>
+</body>
+
+</html>
